4f1a4259a54e23e4be4e6f0939a8880a715b70ae
Ma77an <Matang99@gmail.com>
2022-11-15 19:01:44 +0200

    skeletone

---
+++ parser.py
@@ -0,0 +1,145 @@
+import math
+import re
+
+from numpy import double
+from abc import ABC, abstractmethod
+
+
+class Expression(ABC):
+    @abstractmethod
+    def calc(self) -> double:
+        pass
+
+
+# implement the classes here
+class Num(Expression):
+    def __init__(self, x) -> None:
+        self._x = x
+
+    def calc(self) -> double:
+        return self._x
+
+
+class BinExp(Expression):
+    def calc(self) -> double:
+        pass
+
+    def __init__(self, left, right):
+        self._left = left
+        self._right = right
+
+
+class Plus(BinExp):
+    def __init__(self, left, right):
+        super().__init__(left, right)
+        self._left = left
+        self._right = right
+
+    def calc(self) -> double:
+        return self._left.calc() + self._right.calc()
+
+
+class Minus(BinExp):
+    def __init__(self, left, right):
+        super().__init__(left, right)
+        self._left = left
+        self._right = right
+
+    def calc(self) -> double:
+        return self._left.calc() - self._right.calc()
+
+
+class Mul(BinExp):
+    def __init__(self, left, right):
+        super().__init__(left, right)
+        self._left = left
+        self._right = right
+
+    def calc(self) -> double:
+        return self._left.calc() * self._right.calc()
+
+
+class Div(BinExp):
+    def __init__(self, left, right):
+        super().__init__(left, right)
+        self._left = left
+        self._right = right
+
+    def calc(self) -> double:
+        return self._left.calc() / self._right.calc()
+
+
+# implement the parser function here
+def parser(expression) -> float:
+    # return 0.0
+    queue = []
+    stack = []
+    stackExp = []
+
+    def is_number(str):
+        try:
+            int(str)
+            return True
+        except ValueError:
+            return False
+
+    split = re.findall("[+/*()-]|\d+", expression)
+    splits = []
+    i=0
+    while i<(len(split) - 1):
+        if split[i] == "(" and split[i + 1] == "-":
+            splits.append(split[i + 1] + split[i + 2])
+            i += 4
+        else:
+            splits.append(split[i])
+            i+=1
+    splits.append(split[len(split) - 1])
+
+    for s in splits:
+        if is_number(s):
+            queue.append(s)
+        else:
+            match s:
+                case "/":
+                    stack.append(s)
+                case "*":
+                    stack.append(s)
+                case "(":
+                    stack.append(s)
+                case "+":
+                    while (len(stack) > 0) and (stack[-1] == "*" or stack[-1] == "/"):
+                        queue.append(stack.pop())
+                    stack.append(s)
+                case "-":
+                    while (len(stack) > 0) and (stack[-1] == "*" or stack[-1] == "/"):
+                        queue.append(stack.pop())
+                    stack.append(s)
+                case ")":
+                    while (len(stack) > 0) and (not stack[len(stack) - 1] == "("):
+                        queue.append(stack.pop())
+                    if len(stack) > 0 and stack[-1]=="(":
+                        stack.pop()
+
+    while len(stack) != 0:
+        queue.append(stack.pop())
+
+    for st in queue:
+        if is_number(st):
+            stackExp.append(Num(int(st)))
+        else:
+            right = stackExp.pop()
+            left = stackExp.pop()
+
+            match st:
+                case "/":
+                    stackExp.append(Div(left, right))
+                case "*":
+                    stackExp.append(Mul(left, right))
+                case "+":
+                    stackExp.append(Plus(left, right))
+                case "-":
+                    stackExp.append(Minus(left, right))
+
+    result = math.floor(double(stackExp.pop().calc() * 1000)) / 1000.0
+    print(result)
+    return result
